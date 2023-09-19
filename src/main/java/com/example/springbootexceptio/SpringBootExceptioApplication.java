12b8a0da9dbfc92aa67ee257871e3692405c6564
hamzeh-pourshabanan <hamzeh.p89@gmail.com>
2023-09-19 18:58:28 +0330

    Initial commit

---
+++ src/main/java/com/example/springbootexceptio/SpringBootExceptioApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootexceptio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExceptioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExceptioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springbootexceptio/SpringBootExceptioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootexceptio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExceptioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
